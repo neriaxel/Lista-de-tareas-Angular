90ed669cab8f2e9e97a2fcaf6ab3f38986574de1
neriaxel <axel19_94@hotmail.com>
2022-04-13 11:28:25 -0300

    Se agrega el proyecto

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 12:00 hs",
+        reminder: false 
+    },
+    {
+        id:2,
+        text: "Hacer las compras para la cena",
+        day: "Agosto 5 a las 19:00 hs",
+        reminder: true
+    },
+    {
+        id:3,
+        text: "investigar sobre Boostrap",
+        day:"Agosto 5 a las 20:00hs",
+        reminder:false
+    },
+    {
+        id:4,
+        text: "Leer mi tarea",
+        day: "Agosto 5 a las 21:00hs",
+        reminder:false
+    },
+]
